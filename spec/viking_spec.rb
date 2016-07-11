236a7ec5f13f68622efe9e3084865d9ec55f184b
Adrian Mui <adrian.mui@gmail.com>
2016-07-11 14:35:23 -0400

    viking_spec.rb pick_up weapon method finish, drop_weapon ongoing

--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -2,7 +2,8 @@ require_relative "../lib/viking.rb"
 require 'spec_helper'
 
 describe Viking do
-  let (:v) { Viking.new("Mike", 69)}
+  let (:v) {Viking.new("Mike", 69)}
+  let (:w) {Weapon.new("Bomb Ass")}
 
   describe '#initialize' do
     it 'passing a name to a new viking sets name' do
@@ -14,7 +15,8 @@ describe Viking do
     end
 
     it 'health cannot be overwritten after it was set in initialize' do
-      expect{v.health = 22}.to raise_error
+
+      expect{v.health=22}.to raise_error(NoMethodError)
     end
 
     it 'Viking weapon starts out as nil on default' do
@@ -25,9 +27,28 @@ describe Viking do
   describe '#pick_up_weapon' do
 
     it 'picking up a Weapon sets it as the new weapon' do
-      w = instance_double("Weapon", :name => "Cool Weapon")
       v.pick_up_weapon(w)
-      expect(v.weapon.name).to eq("Cool Weapon")
+      expect(v.weapon.name).to eq("Bomb Ass")
     end
+
+    it 'picking up Non Weapon raises exception' do
+      double_w = double('Weapon', :name => "Not Bomb Ass")
+      
+      expect{v.pick_up_weapon(double_w)}.to raise_error("Can't pick up that thing")
+    end
+
+    it 'Picking up a new weapon replaces the existing weapon' do
+      second_wep = instance_double("Weapon", :name => "Stupid Weapon", :is_a? => Weapon)
+      v.pick_up_weapon(second_wep)
+      expect(v.weapon).to eql(second_wep)
+    end
+  end
+
+  describe '#drop_weapon'
+    it 'dropping a weapon leaves it weaponless' do
+      
+    end
+  end
+
   end
 end
