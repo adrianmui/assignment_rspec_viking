d0fa2a1a5931171e8b250287a09750674502553c
Mike Lee <asackofwheat@gmail.com>
2016-07-11 13:20:50 -0400

    first commit

--- spec/bow_spec.rb
+++ spec/bow_spec.rb
@@ -1,1 +1,12 @@
-# Your code here
+require_relative "../lib/weapons/bow.rb"
+require 'spec_helper'
+
+describe Bow do
+
+  let(:b){Bow.new}
+
+  it 'should read the bow count' do
+    expect(b.arrows).to_not raise
+  end
+
+end
