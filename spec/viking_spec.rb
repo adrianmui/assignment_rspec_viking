7c4ac48503ae3ec16ff3907b4f89f6fb0d6b2fea
Adrian Mui <adrian.mui@gmail.com>
2016-07-11 13:45:16 -0400

    viking_spec.rb

--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -1,1 +1,20 @@
-# Your code here
+require_relative "../lib/viking.rb"
+require 'spec_helper'
+
+describe Viking do
+  let (:v) { Viking.new("Mike", 69)}
+
+  describe '#initialize' do
+    it 'passing a name to a new viking sets name' do
+      expect(v.name).to eq("Mike")
+    end
+
+    it 'passing a health attribute to a new Viking sets health' do
+      expect(v.health).to eq(69)
+    end
+
+
+
+
+  end
+end
